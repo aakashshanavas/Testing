0ca4b3f1c0be41f105d98cea320f18a43cd318ba
aakashshanavas <ashanavas@usf.edu>
2021-02-19 13:49:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace OpenKattis_Test2_D
+{
+    class PushZero
+    {
+        static void Main(string[] args)
+        {
+
+            //int[] Number = new int[5];
+            int[] Number = { 2, 2, 1 };
+            int ArrLength = Number.Length;
+             
+            int finalNo = 0;
+            /*for(int i = 0;i< ArrLength; i++)
+            {
+                Number[i] = Convert.ToInt32(Console.ReadLine());
+            }*/
+            for (int i = 0; i < ArrLength; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < ArrLength; j++)
+                {
+                    if ( i != j )
+                    {
+                        if (Number[i] == Number[j])
+                            count++;
+                    }
+                }
+                if (count == 0)
+                {
+                    finalNo = Number[i];
+                    Console.WriteLine(finalNo);
+                }
+            }
+            
+
+
+        }
+    }
+}
